256eef092060439980d929f2db29fc4d41f31015
manikanta639 <manikantanagubandi9@gmail.com>
2025-10-10 16:46:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,67 @@
 // Sample Product Data
 const products = [
   /*
-  { 
-    id: 22, name: "", category: "", image: "", 
-    link: ""
-  },{ 
-    id: 21, name: "", category: "", image: "", 
-    link: ""
-  },{ 
-    id: 20, name: "", category: "", image: "", 
-    link: ""
-  },{ 
-    id: 19, name: "", category: "", image: "", 
-    link: ""
-  },{ 
-    id: 18, name: "", category: "", image: "", 
-    link: ""
-  },{ 
-    id: 17, name: "", category: "", image: "", 
-    link: ""
-  },{ 
-    id: 16, name: "", category: "", image: "", 
-    link: ""
-  },*/
-  { 
-    id: 15, name: "Sewing Machine", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/sewing-machine/b/s/d/multifunctional-dual-speed-portable-sewing-machine-with-light-original-imah5r2fzfqcs3gy.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/mQm3Gn"
-  },
-  { 
-    id: 14, name: "Gps Tracker", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/gps-device/e/5/a/gf-08-mini-portable-real-time-personal-and-vehicle-gps-tracker-original-imahgeqvfhj7rhhu.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/iy5DTt"
-  },{ 
-    id: 13, name: "Car & Bike Stickers", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/car-sticker/5/p/3/20-20-pcs-multicolor-reflective-glowing-car-wheel-stickers-8-original-imahbpx9v7ba3gjf.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/IqBMhM"
-  },
-  { 
-    id: 12, name: "Exhaust Fan", category: "home appliances", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/exhaust-fan/c/9/l/8-200mm-inline-duct-fan-silent-mix-flow-circular-inline-fan-original-imahfp4exhadspzy.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/sE85Rz"
-  },{ 
-    id: 11, name: "Decorative Wallpaper", category: "home appliances", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/wallpaper/2/y/0/70-77-1-pack-of-1-gold-foamsheet-1pc-a-ingorala-original-imahdq5gtaa4zz8x.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/5RitVE"
-  },
-  { 
-    id: 10, name: "Thermal Laminator", category: "Electronics", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/lamination-machine/x/l/v/9-05-4-98-lm-mini-vms-original-imahdh5vcejpmx2a.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/5V9YNQ"
-  },
-  { 
-    id: 9, name: "Decoration Lights", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/rice-light/g/2/3/280-string-3-05-1-hdrl-new-10x10-fountain-multi-01-home-delight-original-imah4kfhdszs5cnh.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/YGaj29"
-  },
-  { 
-    id: 8, name: "Sealing Machine", category: "home appliances", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/heat-sealer/f/e/1/10-50-portable-mini-food-sealing-machine-with-cutter-magnet-usb-original-imahg2d5a3pguyyz.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/LeNDeg"
-  },
-  { 
-    id: 7, name: "Number Lock", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/lock/s/5/u/combination-combination-lock-10-digit-push-button-combination-original-imah4ky2crkcp67n.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/3xSoet"
-  },
-  { 
-    id: 6, name: "Wire Stripper", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/plier/h/z/t/10-5-in-1-multifunctional-wire-stripper-combination-original-imah5ggstt5ahjaf.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/YhPPww"
-  },
-  { 
-    id: 5, name: "WiFi Bulb Camera", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/home-security-camera/0/g/z/2-wifi-bulb-camera-1080p-with-360-view-wireless-smart-ip-cctv-original-imahfcty5kpwgtjk.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/s0H6mm"
-  },
-  
-  { 
-    id: 4, name: "Stickers & Decal", category: "Accessories", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/car-sticker/j/g/s/2-color-splash-pikachu-sticker-vibrant-artistic-pok-mon-design-original-imahfcvngvbakwvv.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/BRXTMH"
-  },
-  { 
-    id: 3, 
-    name: "String Lights", 
-    category: "gadjets", 
-    image: "https://rukminim2.flixcart.com/image/832/832/xif0q/rice-light/g/i/z/280-string-3-05-1-hdrl-new-10x10-fountain-multi-01-home-delight-original-imah4kfhghpvstnt.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/VJeUBD"
-  },
-  { 
-    id: 2, 
-    name: "Solar Wall Lights", 
-    category: "gadjets", 
-    image: "https://rukminim2.flixcart.com/image/832/832/xif0q/solar-light-set/g/b/k/1-hhsol-99-6-rgb-1-8-homehop-original-imahfcuducj6msgv.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/Nx9GFJ"
-  },
-  { 
-    id: 1, 
-    name: "Led Light For Bike", 
-    category: "gadjets", 
-    image: "https://rukminim2.flixcart.com/image/832/832/xif0q/shopsy-light/0/q/8/colours-7-multiple-flashing-pattern-led-motorcycle-bike-pack-of-original-imagrmcrbnxy3jwv.jpeg?q=70&crop=false", 
-    link: "https://fkrt.co/cs4UBy"
-  },
+  { id: 60, name: "", category: "", image: "", link: ""},
+  { id: 59, name: "", category: "", image: "", link: ""},
+  { id: 58, name: "", category: "", image: "", link: ""},
+  { id: 57, name: "", category: "", image: "", link: ""},
+  { id: 56, name: "", category: "", image: "", link: ""},
+  { id: 55, name: "", category: "", image: "", link: ""},
+  { id: 54, name: "", category: "", image: "", link: ""},
+  { id: 53, name: "", category: "", image: "", link: ""},
+  { id: 52, name: "", category: "", image: "", link: ""},
+  { id: 51, name: "", category: "", image: "", link: ""},
+  { id: 50, name: "", category: "", image: "", link: ""},
+  { id: 49, name: "", category: "", image: "", link: ""},
+  { id: 48, name: "", category: "", image: "", link: ""},
+  { id: 47, name: "", category: "", image: "", link: ""},
+  { id: 46, name: "", category: "", image: "", link: ""},
+  { id: 45, name: "", category: "", image: "", link: ""},
+  { id: 44, name: "", category: "", image: "", link: ""},
+  { id: 43, name: "", category: "", image: "", link: ""},
+  { id: 42, name: "", category: "", image: "", link: ""},
+  { id: 41, name: "", category: "", image: "", link: ""},
+  { id: 40, name: "", category: "", image: "", link: ""},
+  { id: 39, name: "", category: "", image: "", link: ""},
+  { id: 38, name: "", category: "", image: "", link: ""},
+  { id: 37, name: "", category: "", image: "", link: ""},
+  { id: 36, name: "", category: "", image: "", link: ""},
+  { id: 35, name: "", category: "", image: "", link: ""},
+  { id: 34, name: "", category: "", image: "", link: ""},
+  { id: 33, name: "", category: "", image: "", link: ""},
+  { id: 32, name: "", category: "", image: "", link: ""},
+  { id: 31, name: "", category: "", image: "", link: ""},
+  { id: 30, name: "", category: "", image: "", link: ""},
+  { id: 29, name: "", category: "", image: "", link: ""},
+  { id: 28, name: "", category: "", image: "", link: ""},
+  { id: 27, name: "", category: "", image: "", link: ""},
+  { id: 26, name: "", category: "", image: "", link: ""},
+  { id: 25, name: "", category: "", image: "", link: ""},
+  { id: 24, name: "", category: "", image: "", link: ""},
+  { id: 23, name: "", category: "", image: "", link: ""},
+  { id: 22, name: "", category: "", image: "", link: ""},
+  { id: 21, name: "", category: "", image: "", link: ""},
+  { id: 20, name: "", category: "", image: "", link: ""},
+  { id: 19, name: "", category: "", image: "", link: ""},
+  { id: 18, name: "", category: "", image: "", link: ""},
+  { id: 17, name: "", category: "", image: "", link: ""},
+  */
+  { id: 16, name: "Neck Fan", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/usb-gadget/o/5/y/super-quiet-3-speeds-personal-wearable-hemovia-original-imahdqf9mjktgyze.jpeg?q=70&crop=false", link: "https://fkrt.co/qx7FZ6"},
+  { id: 15, name: "Sewing Machine", category: "home appliances", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/sewing-machine/b/s/d/multifunctional-dual-speed-portable-sewing-machine-with-light-original-imah5r2fzfqcs3gy.jpeg?q=70&crop=false", link: "https://fkrt.co/mQm3Gn"},
+  { id: 14, name: "Gps Tracker", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/gps-device/e/5/a/gf-08-mini-portable-real-time-personal-and-vehicle-gps-tracker-original-imahgeqvfhj7rhhu.jpeg?q=70&crop=false", link: "https://fkrt.co/iy5DTt"},
+  { id: 13, name: "Car & Bike Stickers", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/car-sticker/5/p/3/20-20-pcs-multicolor-reflective-glowing-car-wheel-stickers-8-original-imahbpx9v7ba3gjf.jpeg?q=70&crop=false", link: "https://fkrt.co/IqBMhM"},
+  { id: 12, name: "Exhaust Fan", category: "home appliances", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/exhaust-fan/c/9/l/8-200mm-inline-duct-fan-silent-mix-flow-circular-inline-fan-original-imahfp4exhadspzy.jpeg?q=70&crop=false", link: "https://fkrt.co/sE85Rz"},
+  { id: 11, name: "Decorative Wallpaper", category: "home appliances", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/wallpaper/2/y/0/70-77-1-pack-of-1-gold-foamsheet-1pc-a-ingorala-original-imahdq5gtaa4zz8x.jpeg?q=70&crop=false", link: "https://fkrt.co/5RitVE"},
+  { id: 10, name: "Thermal Laminator", category: "Electronics", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/lamination-machine/x/l/v/9-05-4-98-lm-mini-vms-original-imahdh5vcejpmx2a.jpeg?q=70&crop=false", link: "https://fkrt.co/5V9YNQ"},
+  { id: 9, name: "Decoration Lights", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/rice-light/g/2/3/280-string-3-05-1-hdrl-new-10x10-fountain-multi-01-home-delight-original-imah4kfhdszs5cnh.jpeg?q=70&crop=false", link: "https://fkrt.co/YGaj29"},
+  { id: 8, name: "Sealing Machine", category: "home appliances", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/heat-sealer/f/e/1/10-50-portable-mini-food-sealing-machine-with-cutter-magnet-usb-original-imahg2d5a3pguyyz.jpeg?q=70&crop=false", link: "https://fkrt.co/LeNDeg"},
+  { id: 7, name: "Number Lock", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/lock/s/5/u/combination-combination-lock-10-digit-push-button-combination-original-imah4ky2crkcp67n.jpeg?q=70&crop=false", link: "https://fkrt.co/3xSoet"},
+  { id: 6, name: "Wire Stripper", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/plier/h/z/t/10-5-in-1-multifunctional-wire-stripper-combination-original-imah5ggstt5ahjaf.jpeg?q=70&crop=false", link: "https://fkrt.co/YhPPww"},
+  { id: 5, name: "WiFi Bulb Camera", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/home-security-camera/0/g/z/2-wifi-bulb-camera-1080p-with-360-view-wireless-smart-ip-cctv-original-imahfcty5kpwgtjk.jpeg?q=70&crop=false", link: "https://fkrt.co/s0H6mm"},
+  { id: 4, name: "Stickers & Decal", category: "Accessories", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/car-sticker/j/g/s/2-color-splash-pikachu-sticker-vibrant-artistic-pok-mon-design-original-imahfcvngvbakwvv.jpeg?q=70&crop=false", link: "https://fkrt.co/BRXTMH"},
+  { id: 3, name: "String Lights", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/rice-light/g/i/z/280-string-3-05-1-hdrl-new-10x10-fountain-multi-01-home-delight-original-imah4kfhghpvstnt.jpeg?q=70&crop=false", link: "https://fkrt.co/VJeUBD"},
+  { id: 2, name: "Solar Wall Lights", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/solar-light-set/g/b/k/1-hhsol-99-6-rgb-1-8-homehop-original-imahfcuducj6msgv.jpeg?q=70&crop=false", link: "https://fkrt.co/Nx9GFJ"},
+  { id: 1, name: "Led Light For Bike", category: "gadjets", image: "https://rukminim2.flixcart.com/image/832/832/xif0q/shopsy-light/0/q/8/colours-7-multiple-flashing-pattern-led-motorcycle-bike-pack-of-original-imagrmcrbnxy3jwv.jpeg?q=70&crop=false", link: "https://fkrt.co/cs4UBy"},
 ];
 
 // ✅ Combo Products Data (Scalable) — Do Not Remove
@@ -255,6 +226,7 @@ window.onload = function() {
   renderProducts(products);
   document.body.style.opacity = 1;
 };
+
 
 
 
